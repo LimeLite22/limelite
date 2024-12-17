51f4be0a517c1a17efc7a5cbe34538e8a910cf4d
Andrewarhypchuk <andrewarhypchuk@gmail.com>
2024-12-17 14:56:00 +0200

    fix

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const path = require("path");
+const { enforce } = require("express-sslify");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware для перенаправлення HTTP -> HTTPS
-app.use((req, res, next) => {
-  if (req.headers["x-forwarded-proto"] !== "https") {
-    return res.redirect(`https://${req.headers.host}${req.url}`);
-  }
-  next();
-});
+// Використовуй middleware для HTTPS редіректу
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-// Обслуговування статичних файлів з папки build
 app.use(express.static(path.join(__dirname, "build")));
 
-// Обробка всіх інших маршрутів (для React Router)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
