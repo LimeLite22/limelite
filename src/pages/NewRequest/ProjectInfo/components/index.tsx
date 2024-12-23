d17b0c1779f3d647b58cfd765630a12eab75d2a5
Andrewarhypchuk <andrewarhypchuk@gmail.com>
2024-12-23 16:29:19 +0200

    small color fix

--- src/pages/NewRequest/ProjectInfo/components/index.tsx
+++ src/pages/NewRequest/ProjectInfo/components/index.tsx
@@ -5,5 +5,4 @@ export { default as ProjectTone } from "./ProjectTone/ProjectTone";
 export { default as ProjectType } from "./ProjectType";
 export { default as RequestType } from "./RequestType";
 export { default as TargetAudienceBox } from "./TargetAudienceBox";
-export { default as ProjectToneBox } from "./ProjectToneBox";
 export { default as ShotListBox } from "./ShotListBox";
