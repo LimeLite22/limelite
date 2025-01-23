ed9adfb4e9e62de2926dd1c08bc7fd88d0d70e07
Andrewarhypchuk <andrewarhypchuk@gmail.com>
2025-01-23 22:13:04 +0200

    update design for walk throough

--- src/pages/WalkThrough/WalkThrough.tsx
+++ src/pages/WalkThrough/WalkThrough.tsx
@@ -41,196 +41,193 @@ const WalkThrough = () => {
     <div className={styles.walkThroughСontainer}>
       <Header />
       <div className={styles.walkThroughСontainer__content}>
-        {width > 768 && (
-          <>
-            <img
-              className={styles.walkThroughСontainer__content_swiperImage}
-              src={
-                slideNumber === 0
-                  ? SwiperFoto1
-                  : slideNumber === 1
-                    ? SwiperFoto2
-                    : SwiperFoto3
-              }
-              alt={"SwiperFoto1"}
-            />
-            <img
-              className={styles.walkThroughСontainer__content_swiperImageMobile}
-              src={
-                slideNumber === 0
-                  ? SwiperFoto1Mob
-                  : slideNumber === 1
-                    ? SwiperFoto2Mob
-                    : SwiperFoto3Mob
-              }
-              alt={"SwiperFoto1"}
-            />
-            <div className={styles.walkThroughСontainer__content_header}>
-              How it Works{" "}
-            </div>
-            <div className={styles.walkThroughСontainer__content_text}>
-              Lorem Ispum Dollar is a Dummy Text That is Used in place of a text
-              that is used in place of a text Lorem Ispum Dollar
-            </div>
-          </>
-        )}
-        {width <= 768 && (
-          <Swiper
-            modules={[Navigation, Pagination, Scrollbar, A11y, Keyboard]}
-            onBeforeInit={(swiper: SwiperType) => {
-              swiperRef.current = swiper;
-            }}
-            onSlideChange={(swiper: SwiperType) => {
-              setSlideNumber(swiper.realIndex);
-            }}
-            allowTouchMove={true}
-            spaceBetween={16}
-            draggable={true}
-            slidesPerView={1}
-            navigation
-            className={styles.walkThroughСontainer_swiper}
-          >
-            <ChangeSlide position={slideNumber} />
-            <SwiperSlide className={styles.walkThroughСontainer_swiper_item}>
+        <div className={styles.walkThroughСontainer__container}>
+          {width > 768 && (
+            <>
               <img
                 className={styles.walkThroughСontainer__content_swiperImage}
-                src={SwiperFoto1}
+                src={
+                  slideNumber === 0
+                    ? SwiperFoto1
+                    : slideNumber === 1
+                      ? SwiperFoto2
+                      : SwiperFoto3
+                }
                 alt={"SwiperFoto1"}
               />
+              <img
+                className={styles.walkThroughСontainer__content_swiperImageMobile}
+                src={
+                  slideNumber === 0
+                    ? SwiperFoto1Mob
+                    : slideNumber === 1
+                      ? SwiperFoto2Mob
+                      : SwiperFoto3Mob
+                }
+                alt={"SwiperFoto1"}
+              />
+              <div className={styles.walkThroughСontainer__content_header}>
+                How it Works{" "}
+              </div>
+              <div className={styles.walkThroughСontainer__content_text}>
+                Lorem Ispum Dollar is a Dummy Text That is Used in place of a text
+                that is used in place of a text Lorem Ispum Dollar
+              </div>
+            </>
+          )}
+          {width <= 768 && (
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y, Keyboard]}
+              onBeforeInit={(swiper: SwiperType) => {
+                swiperRef.current = swiper;
+              }}
+              onSlideChange={(swiper: SwiperType) => {
+                setSlideNumber(swiper.realIndex);
+              }}
+              allowTouchMove={true}
+              spaceBetween={16}
+              draggable={true}
+              slidesPerView={1}
+              navigation
+              className={styles.walkThroughСontainer_swiper}
+            >
+              <ChangeSlide position={slideNumber} />
+              <SwiperSlide className={styles.walkThroughСontainer_swiper_item}>
+                <img
+                  className={styles.walkThroughСontainer__content_swiperImage}
+                  src={SwiperFoto1}
+                  alt={"SwiperFoto1"}
+                />
 
-              <img
-                className={
-                  styles.walkThroughСontainer__content_swiperImageMobile
-                }
-                src={SwiperFoto1Mob}
-                alt={"SwiperFoto1"}
-              />
-              <div className={styles.walkThroughСontainer__content_header}>
-                How it Works{" "}
-              </div>
-              <div className={styles.walkThroughСontainer__content_text}>
-                Lorem Ispum Dollar is a Dummy Text That is Used in place of a
-                text that is used in place of a text Lorem Ispum Dollar
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className={styles.walkThroughСontainer_swiper_item}>
-              <img
-                className={styles.walkThroughСontainer__content_swiperImage}
-                src={SwiperFoto2}
-                alt={"SwiperFoto1"}
-              />
-              <img
-                className={
-                  styles.walkThroughСontainer__content_swiperImageMobile
-                }
-                src={SwiperFoto2Mob}
-                alt={"SwiperFoto1"}
-              />
-              <div className={styles.walkThroughСontainer__content_header}>
-                How it Works{" "}
-              </div>
-              <div className={styles.walkThroughСontainer__content_text}>
-                Lorem Ispum Dollar is a Dummy Text That is Used in place of a
-                text that is used in place of a text Lorem Ispum Dollar
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className={styles.walkThroughСontainer_swiper_item}>
-              <img
-                className={styles.walkThroughСontainer__content_swiperImage}
-                src={SwiperFoto3}
-                alt={"SwiperFoto1"}
-              />
-              <img
-                className={
-                  styles.walkThroughСontainer__content_swiperImageMobile
-                }
-                src={SwiperFoto3Mob}
-                alt={"SwiperFoto1"}
-              />
-              <div className={styles.walkThroughСontainer__content_header}>
-                How it Works{" "}
-              </div>
-              <div className={styles.walkThroughСontainer__content_text}>
-                Lorem Ispum Dollar is a Dummy Text That is Used in place of a
-                text that is used in place of a text Lorem Ispum Dollar
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        )}
-        <div className={styles.walkThroughСontainer__content_navigation}>
-          <div
-            onClick={() => setSlideNumber(0)}
-            className={`${
-              styles.walkThroughСontainer__content_navigation_button
-            } ${
-              slideNumber === 0
-                ? styles.walkThroughСontainer__content_navigation_button_active
-                : ""
-            } `}
-          >
-            {" "}
-          </div>
-          <div
-            onClick={() => setSlideNumber(1)}
-            className={`${
-              styles.walkThroughСontainer__content_navigation_button
-            } ${
-              slideNumber === 1
-                ? styles.walkThroughСontainer__content_navigation_button_active
-                : ""
-            } `}
-          >
-            {" "}
-          </div>
-          <div
-            onClick={() => setSlideNumber(2)}
-            className={`${
-              styles.walkThroughСontainer__content_navigation_button
-            } ${
-              slideNumber === 2
-                ? styles.walkThroughСontainer__content_navigation_button_active
-                : ""
-            } `}
-          >
-            {" "}
-          </div>
-        </div>
-        <div className={styles.walkThroughСontainer__content_buttonContainer}>
-          {slideNumber === 0 && (
+                <img
+                  className={
+                    styles.walkThroughСontainer__content_swiperImageMobile
+                  }
+                  src={SwiperFoto1Mob}
+                  alt={"SwiperFoto1"}
+                />
+                <div className={styles.walkThroughСontainer__content_header}>
+                  How it Works{" "}
+                </div>
+                <div className={styles.walkThroughСontainer__content_text}>
+                  Lorem Ispum Dollar is a Dummy Text That is Used in place of a
+                  text that is used in place of a text Lorem Ispum Dollar
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className={styles.walkThroughСontainer_swiper_item}>
+                <img
+                  className={styles.walkThroughСontainer__content_swiperImage}
+                  src={SwiperFoto2}
+                  alt={"SwiperFoto1"}
+                />
+                <img
+                  className={
+                    styles.walkThroughСontainer__content_swiperImageMobile
+                  }
+                  src={SwiperFoto2Mob}
+                  alt={"SwiperFoto1"}
+                />
+                <div className={styles.walkThroughСontainer__content_header}>
+                  How it Works{" "}
+                </div>
+                <div className={styles.walkThroughСontainer__content_text}>
+                  Lorem Ispum Dollar is a Dummy Text That is Used in place of a
+                  text that is used in place of a text Lorem Ispum Dollar
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className={styles.walkThroughСontainer_swiper_item}>
+                <img
+                  className={styles.walkThroughСontainer__content_swiperImage}
+                  src={SwiperFoto3}
+                  alt={"SwiperFoto1"}
+                />
+                <img
+                  className={
+                    styles.walkThroughСontainer__content_swiperImageMobile
+                  }
+                  src={SwiperFoto3Mob}
+                  alt={"SwiperFoto1"}
+                />
+                <div className={styles.walkThroughСontainer__content_header}>
+                  How it Works{" "}
+                </div>
+                <div className={styles.walkThroughСontainer__content_text}>
+                  Lorem Ispum Dollar is a Dummy Text That is Used in place of a
+                  text that is used in place of a text Lorem Ispum Dollar
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          )}
+          <div className={styles.walkThroughСontainer__content_navigation}>
             <div
-              className={
-                styles.walkThroughСontainer__content_buttonContainer_skipButton
-              }
+              onClick={() => setSlideNumber(0)}
+              className={`${styles.walkThroughСontainer__content_navigation_button
+                } ${slideNumber === 0
+                  ? styles.walkThroughСontainer__content_navigation_button_active
+                  : ""
+                } `}
+            >
+              {" "}
+            </div>
+            <div
+              onClick={() => setSlideNumber(1)}
+              className={`${styles.walkThroughСontainer__content_navigation_button
+                } ${slideNumber === 1
+                  ? styles.walkThroughСontainer__content_navigation_button_active
+                  : ""
+                } `}
+            >
+              {" "}
+            </div>
+            <div
               onClick={() => setSlideNumber(2)}
+              className={`${styles.walkThroughСontainer__content_navigation_button
+                } ${slideNumber === 2
+                  ? styles.walkThroughСontainer__content_navigation_button_active
+                  : ""
+                } `}
             >
-              Skip
+              {" "}
             </div>
-          )}
-          {slideNumber !== 0 && (
+          </div>
+          <div className={styles.walkThroughСontainer__content_buttonContainer}>
+            {slideNumber === 0 && (
+              <div
+                className={
+                  styles.walkThroughСontainer__content_buttonContainer_skipButton
+                }
+                onClick={() => setSlideNumber(2)}
+              >
+                Skip
+              </div>
+            )}
+            {slideNumber !== 0 && (
+              <div
+                className={
+                  styles.walkThroughСontainer__content_buttonContainer_skipButton
+                }
+                onClick={() => slideNumber > 0 && setSlideNumber(slideNumber - 1)}
+              >
+                Back
+              </div>
+            )}
             <div
               className={
-                styles.walkThroughСontainer__content_buttonContainer_skipButton
+                styles.walkThroughСontainer__content_buttonContainer_continueButton
               }
-              onClick={() => slideNumber > 0 && setSlideNumber(slideNumber - 1)}
+              onClick={() => {
+                if (slideNumber === 2) {
+                  navigate("/selectProfile");
+                } else {
+                  setSlideNumber(slideNumber + 1);
+                }
+              }}
             >
-              Back
+              Next
             </div>
-          )}
-          <div
-            className={
-              styles.walkThroughСontainer__content_buttonContainer_continueButton
-            }
-            onClick={() => {
-              if (slideNumber === 2) {
-                navigate("/selectProfile");
-              } else {
-                setSlideNumber(slideNumber + 1);
-              }
-            }}
-          >
-            Next
           </div>
         </div>
+
       </div>
     </div>
   );
